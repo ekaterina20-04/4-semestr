9f92ce3cd2a8e71930a57ac4d65982b88aba8a2c
Катюшка <vodopianovae@mail.ru>
2024-04-05 01:17:01 +0300

    add works of hash table

---
+++ hesh_table.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <set>
+#include <unordered_set>
+using namespace std; 
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	unordered_set<int> numbers;
+	cout << "Введите числа через enter: ";
+
+	int num;
+	while (cin >> num) {
+		if (num > 0) {
+			numbers.insert(num);
+		}
+		else if (num < 0) {
+			numbers.erase(-num);
+		}
+		else {
+			set<int> sorted_numbers(numbers.begin(), numbers.end());
+			for (int n : sorted_numbers) {
+				cout << n << " ";
+			}
+			break;
+		}
+	}
+
+	return 0;
+}
